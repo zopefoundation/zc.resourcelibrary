3dc95e2c60c6e66ba96bf3de0c8b7db4e5367c62
Martijn Faassen <faassen@startifact.com>
2006-04-05 10:55:15 +0000

    * Add in setup.py so that egg can be created for zc.resourcelibrary.
    * make zc/__init__.py a namespace package.
    
    * add in svn:ignore to ignore egg-generated files.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zc.resourcelibrary",
+    version="0.1",
+    packages=find_packages('src', exclude=["*.tests", "*.ftests"]),
+    
+    package_dir= {'':'src'},
+    
+    namespace_packages=['zc'],
+    package_data = {
+    '': ['*.txt', '*.zcml'],
+    },
+
+    zip_safe=False,
+    author='Zope Project',
+    author_email='zope3-dev@zope.org',
+    description="""\
+The resource library is a Zope 3 extension that is designed to make the
+inclusion of JavaScript, CSS, and other resources easy, cache-friendly,
+and component-friendly.
+""",
+    license='ZPL',
+    keywords="zope zope3",
+    )
