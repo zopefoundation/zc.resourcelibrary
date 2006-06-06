779049c352d5aa19168aa8b6a61ce4355cb170c6
Martijn Faassen <faassen@startifact.com>
2006-06-06 10:31:37 +0000

    Prepare for 0.5.1 release, fixing deprecation warnings.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="zc.resourcelibrary",
-    version="0.5",
+    version="0.5.1",
     packages=find_packages('src', exclude=["*.tests", "*.ftests"]),
     
     package_dir= {'':'src'},
