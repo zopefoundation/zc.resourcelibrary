7f0920b068ec48febfa8c352384f1a3a606c62af
Thomas Lotze <tl@gocept.com>
2007-12-07 10:47:08 +0000

    set release version to 0.8.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zc.resourcelibrary',
-      version = '1.0dev',
+      version = '0.8.2',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Post-rendering Resource Inclusion',
