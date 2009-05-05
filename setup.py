74ff8f6ea028f9a7b6dc895848a0d69f631e116e
Malthe Borch <mborch@gmail.com>
2009-05-05 13:04:43 +0000

    Preparing release.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zc.resourcelibrary',
-      version = '1.1.0dev',
+      version = '1.1.0',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Post-rendering Resource Inclusion',
