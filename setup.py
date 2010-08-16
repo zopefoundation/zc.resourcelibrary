ebfb085cff56d466aa1f9772aacea8e1c90c3d93
Patrick Strawderman <patrick@kilink.net>
2010-08-16 20:22:42 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zc.resourcelibrary',
-      version = '1.2.1dev',
+      version = '1.2.1',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Post-rendering Resource Inclusion',
