61eb0fe8d1a4a45a40961310770bf02986a43faa
Jason Madden <jamadden@gmail.com>
2017-05-23 08:04:42 -0500

    Back to development: 2.0.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
         return f.read()
 
 setup(name='zc.resourcelibrary',
-      version='2.0.0',
+      version='2.0.1.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Post-rendering Resource Inclusion',
